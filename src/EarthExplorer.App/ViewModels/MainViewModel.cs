2ae685aa6f22ed1f9334dde1aabea3173d3a953d
saveliyshap22-crypto <saveliyshap22@gmail.com>
2026-07-13 10:29:30 +0300

    fix: import HTTP exception type in WPF app

--- src/EarthExplorer.App/ViewModels/MainViewModel.cs
+++ src/EarthExplorer.App/ViewModels/MainViewModel.cs
@@ -1,6 +1,7 @@
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
+using System.Net.Http;
 using System.Windows.Threading;
 using EarthExplorer.App.Services;
 using EarthExplorer.Core.Models;
